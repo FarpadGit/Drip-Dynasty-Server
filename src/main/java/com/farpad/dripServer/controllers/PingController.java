9c6724d75ccf18bb433247fde80aa06e20410a26
FarpadGit <farpadmail@gmail.com>
2025-10-27 19:27:40 +0100

    Create PingController.java

---
+++ src/main/java/com/farpad/dripServer/controllers/PingController.java
@@ -0,0 +1,17 @@
+package com.farpad.dripServer.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class PingController {
+    @RequestMapping(value = "/ping", method = RequestMethod.HEAD)
+    public ResponseEntity<Void> ping(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
